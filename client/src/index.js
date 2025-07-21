6923694441041c5b19ca3c297161e4347b1e474c
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-07-21 22:39:43 +0530

    Set up environment variables, database connection, and server initialization

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,22 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config();
+
+connectDB().then(() => {
+  //db connect , now start server
+  const port = process.env.PORT || 5000;
+  try {
+    app.on("error", (error) => {
+      console.log("Application : ", error.message);
+      throw error;
+    });
+
+    app.listen(port, () => {
+      console.log("Server is running on port : ", port);
+    });
+  } catch (error) {
+    console.log("MongoDB connection failed !! : ", error.message);
+    process.exit(1);
+  }
+});
