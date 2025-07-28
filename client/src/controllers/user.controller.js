3db00ce1ef7a71f418a3aef76be7b0b34a8f89fd
Prafull Shinde <prafullshinde7007@gmail.com>
2025-07-28 13:43:30 +0530

    auth controller implemented

---
+++ client/src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken"
+import mongoose from 'mongoose'
+import asyncHandler from '../utils/asyncHandlers.js'
+import ApiResponse from '../utils/apiResponse.js'
+import ApiError from '../utils/apiError.js'
+
+
+
+
--- client/src/models/user.model.js
+++ client/src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 const UserSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
       unique: true,
@@ -23,7 +23,7 @@ const UserSchema = new Schema(
       trim: true,
     },
 
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -57,5 +57,46 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+     
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+UserSchema.methods.isPasswordCorrect = async function(password){
+ return await bcrypt.compare(password,this.password)
+}
+
+UserSchema.methods.generateAccessToken = function(){
+   return jwt.sign(
+    {
+        _id : this._id,
+        email : this.email,
+        userName :this.userName,
+        fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+  ) 
+}
+
+UserSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+    {
+        _id : this._id,
+        email : this.email,
+        userName :this.userName,
+        fullName : this.fullName
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+  ) 
+}
 
 export const User = mongoose.model("User", UserSchema);
