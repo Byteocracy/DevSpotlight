2d63bc3c938728b0d250f1677bf4f3642f6c8c4d
Prafull Shinde <prafullshinde7007@gmail.com>
2025-08-10 16:59:29 +0530

    contribution model updated

--- client/src/models/contribution.model.js
+++ client/src/models/contribution.model.js
@@ -1,14 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
-const contributionSchema = new Schema({
-  project: {
-    type: mongoose.Types.ObjectId,
-    ref: "Project",
+const contributionSchema = new Schema(
+  {
+    project: {
+      type: mongoose.Types.ObjectId,
+      ref: "Project",
+      required: true
+    },
+    contributor: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING"
+    }
   },
-  contributor: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 export const Contribution = mongoose.model("Contribution", contributionSchema);
